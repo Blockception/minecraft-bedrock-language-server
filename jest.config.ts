afe2efda73f34b4b9357bf65c56f23a082fe7e28
DaanV2 <daanverstraten@hotmail.com>
2025-09-28 16:50:36 +0200

    refactor: consoldating everything

---
+++ jest.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coveragePathIgnorePatterns: ["node_modules", "coverage"],
+  coverageProvider: "v8",
+  testEnvironment: "node",
+  roots: ["<rootDir>/packages", "<rootDir>/ide"],
+  moduleFileExtensions: ["ts", "js", "json", "node"],
+  transform: {
+    "^.+\\.ts$": "ts-jest"
+  },
+  testMatch: ["**/test/**/*.test.ts", "**/test/**/*.spec.ts"]
+};
+
+export default config;
