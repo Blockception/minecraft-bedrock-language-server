1d8fe33ec2feec7a2d998559edc684e2700f6aa1
Daan Verstraten <daanverstraten@hotmail.com>
2025-10-30 19:03:30 +0100

    fix: checking if short form check works (#71)

--- packages/bedrock-diagnoser/test/lib/diagnostics/molang/implementation.test.ts
+++ packages/bedrock-diagnoser/test/lib/diagnostics/molang/implementation.test.ts
@@ -11,26 +11,29 @@ describe("Molang", () => {
       const using = new MolangSet();
       const resource = new MolangSet();
 
+      using.assigned.add({ scope: "variable", names: ["foo"], position: 0, type: NodeType.Variable });
+      resource.using.add({ scope: "variable", names: ["foo"], position: 0, type: NodeType.Variable });
+
       diagnose_molang_implementation(
         { id: "animation.example.walk", molang: using },
         { id: "minecraft:sheep", molang: resource },
         diagnoser
       );
 
-      using.assigned.add({ scope: "variable", names: ["foo"], position: 0, type: NodeType.Variable });
-      resource.using.add({ scope: "variable", names: ["foo"], position: 0, type: NodeType.Variable });
-
       diagnoser.expectEmpty();
     });
 
-    it("1 error", () => {
+    it("it should be able to pass with short hand checks", () => {
       const diagnoser = Metadata.withMetadata(TestDiagnoser.create(), { userType: "Entities" } as MolangMetadata);
 
       const using = new MolangSet();
       const resource = new MolangSet();
 
-      using.assigned.add({ scope: "variable", names: ["bar"], position: 0, type: NodeType.Variable });
-      resource.using.add({ scope: "variable", names: ["foo"], position: 0, type: NodeType.Variable });
+      using.assigned.add({ scope: "variable", names: ["foo"], position: 0, type: NodeType.Variable });
+      resource.using.add({ scope: "v", names: ["foo"], position: 0, type: NodeType.Variable });
+
+      using.assigned.add({ scope: "v", names: ["bar"], position: 0, type: NodeType.Variable });
+      resource.using.add({ scope: "variable", names: ["bar"], position: 0, type: NodeType.Variable });
 
       diagnose_molang_implementation(
         { id: "animation.example.walk", molang: using },
@@ -38,7 +41,7 @@ describe("Molang", () => {
         diagnoser
       );
 
-      diagnoser.expectAmount(1);
+      diagnoser.expectEmpty();
     });
 
     it("1 error", () => {
