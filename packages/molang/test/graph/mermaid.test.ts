cc7d5ea455de937fa341e2a55e47923d2fa48711
DaanV2 <2393905+DaanV2@users.noreply.github.com>
2025-12-29 07:58:44 +0100

    fix: tests

--- packages/molang/test/graph/mermaid.test.ts
+++ packages/molang/test/graph/mermaid.test.ts
@@ -2,6 +2,7 @@
 import { parseMolang } from '../../src/molang/syntax/parse';
 import { valid_syntaxes } from '../data/dataset-valid';
 import { generateMermaidDiagram } from '../../src/graph/mermaid';
+import { OffsetWord } from "@blockception/packages-shared";
 
 describe('molang - mermaid - graphs', () => {
   describe('should be able to generate stage diagrams', () => {
